f48e70f445beec58cce6777a5feaac8e7ed073ca
Luiz Augusto <luizbapmarques@gmail.com>
2021-04-12 20:18:18 -0300

    Header Component → ✨ Done!

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
-}
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div>
+        <Link href="/">
+          <img src="/spacetraveling.svg" alt="logo" />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
